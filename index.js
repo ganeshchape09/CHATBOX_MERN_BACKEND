69d7f045162a01c4a9dc08ede4437f6eec225d3a
ganeshchape09 <ganeshchape09@gmail.com>
2024-05-16 03:00:36 +0530

    Commit

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const cookiesParser = require("cookie-parser");
 const { app, server } = require("./socket/index");
 
 // const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://subtle-manatee-0ca198.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(cookiesParser());
 
